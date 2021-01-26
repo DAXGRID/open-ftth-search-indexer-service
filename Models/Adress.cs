75f67af67c71d57c09c62f9b2983e84d48368da0
mihai.tirtara@gmail.com <mihai.tirtara@gmail.com>
2021-01-26 10:28:19 +0100

    Change structure for web api and added RouteNodesController

---
+++ Models/Adress.cs
@@ -0,0 +1,20 @@
+namespace openftth_search_indexer_service.Models
+{
+    public class Address
+    {
+        public string id_lokalId { get; set; }
+        public string door { get; set; }
+        public string floor { get; set; }
+        public string unitAddressDescription { get; set; }
+        public string houseNumberId { get; set; }
+        public int status { get; set; }
+        public string houseNumberDirection { get; set; }
+        public string houseNumberText { get; set; }
+        public string position { get; set; }
+        public string accessAddressDescription { get; set; }
+
+        public string roadName {get;set;}
+        
+
+    }
+}
---
+++ Models/RouteNode.cs
@@ -0,0 +1,10 @@
+using System;
+namespace openftth_search_indexer_service.Models
+{
+    public class RouteNode
+    {
+        public Guid id {get;set;}
+        public int  incrementalId {get;set;}
+        public string name {get;set;}
+    }
+}
