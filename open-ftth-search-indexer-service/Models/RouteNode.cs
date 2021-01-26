70b322c1e1d509a09b3979569486df3380da91da
mihai.tirtara@gmail.com <mihai.tirtara@gmail.com>
2021-01-26 11:45:35 +0100

    Project restructure

---
+++ open-ftth-search-indexer-service/Models/RouteNode.cs
@@ -0,0 +1,10 @@
+using System;
+namespace open_ftth_search_indexer_service.Models
+{
+    public class RouteNode
+    {
+        public Guid id {get;set;}
+        public int  incrementalId {get;set;}
+        public string name {get;set;}
+    }
+}
