9afd2938da648b8d3c9fee86fb3d6c95ec1abd60
mihai.tirtara@gmail.com <mihai.tirtara@gmail.com>
2021-02-02 13:41:03 +0100

    environment variables added

--- open-ftth-search-indexer-service/src/Startup.cs
+++ open-ftth-search-indexer-service/src/Startup.cs
@@ -11,35 +11,48 @@ using Microsoft.Extensions.Hosting;
 using Typesense;
 using Typesense.Setup;
 using System.Collections.Generic;
+using System;
 
 namespace open_ftth_search_indexer_service
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment enviroment)
         {
             Configuration = configuration;
+            WebEnviroment = enviroment;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment WebEnviroment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var apiKey = "Hu52dwsas2AdxdE";
+            var host = "localhost";
+            var port = "8108";
+
+            if (WebEnviroment.IsProduction())
+            {
+                apiKey = Environment.GetEnvironmentVariable("TYPESENSE_APIKEY");
+                host = Environment.GetEnvironmentVariable("TYPESENSE_HOST");
+                port = Environment.GetEnvironmentVariable("TYPESENSE_PORT");
+            }
             
             var node = new Node();
-            node.Host = "localhost";
-            node.Port = "8108";
+            node.Host = host;
+            node.Port = port;
             node.Protocol = "http";
             services.AddTypesenseClient(options =>
                 {
-                    options.ApiKey = "Hu52dwsas2AdxdE";
+                    options.ApiKey = apiKey;
                     options.Nodes = new List<Node>();
                     options.Nodes.Add(node);
                 });
             services.AddControllers();
             services.AddHealthChecks();
-            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,7 +67,7 @@ namespace open_ftth_search_indexer_service
 
             app.UseRouting();
 
-            app.UseHealthChecks("/health", new HealthCheckOptions {ResponseWriter = JsonResponseWriter});
+            app.UseHealthChecks("/health", new HealthCheckOptions { ResponseWriter = JsonResponseWriter });
 
             app.UseAuthorization();
 
@@ -67,8 +80,8 @@ namespace open_ftth_search_indexer_service
         private async Task JsonResponseWriter(HttpContext context, HealthReport report)
         {
             context.Response.ContentType = "application/json";
-            await JsonSerializer.SerializeAsync(context.Response.Body, new {Status = report.Status.ToString()},
-                new JsonSerializerOptions {PropertyNamingPolicy = JsonNamingPolicy.CamelCase});
+            await JsonSerializer.SerializeAsync(context.Response.Body, new { Status = report.Status.ToString() },
+                new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
         }
     }
 }
